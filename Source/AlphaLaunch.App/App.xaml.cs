7f8505e635d8e3d67c5080069b3b668aac2fcec6
Rasmus Kromann-Larsen <rasmus@kromann-larsen.dk>
2012-10-04 21:59:36 +0200

    Added tray icon.

--- Source/AlphaLaunch.App/App.xaml.cs
+++ Source/AlphaLaunch.App/App.xaml.cs
@@ -1,17 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
+using System.Drawing;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Reflection;
 using System.Windows;
+using System.Windows.Forms;
 
 namespace AlphaLaunch.App
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    public partial class App
     {
+        private NotifyIcon _notifyIcon;
+
+        private void ApplicationStartup(object sender, StartupEventArgs e)
+        {
+            _notifyIcon = new NotifyIcon();
+            _notifyIcon.Visible = true;
+
+            using(var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("AlphaLaunch.App.Icon.ico"))
+            {
+                if (stream != null)
+                {
+                    _notifyIcon.Icon = new Icon(stream);
+                }
+            }
+
+            _notifyIcon.Click += (o, args) => Shutdown();
+
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+
+        private void ApplicationExit(object sender, ExitEventArgs e)
+        {
+            _notifyIcon.Visible = false;
+        }
     }
 }
