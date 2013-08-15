aa653a31bc426509920bfe7c8a5bca2cf83ba74e
Rasmus Kromann-Larsen <rasmus@kromann-larsen.dk>
2013-08-15 15:54:09 +0200

    Added a naive fuzzy matcher.

---
+++ Source/AlphaLaunch.Experiments/FuzzyExperiment.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Should;
+using Xunit;
+
+namespace AlphaLaunch.Experiments
+{
+    public class FuzzyExperiment
+    {
+        [Fact]
+        public void SimpleMatches()
+        {
+            AssertMatches("abc", "abc");
+            AssertMatches("a", "abc");
+            AssertMatches("b", "abc");
+            AssertMatches("c", "abc");
+        }
+
+        [Fact]
+        public void SimpleNoMatches()
+        {
+            AssertNoMatches("d", "abc");
+        }
+
+        [Fact]
+        public void Matches()
+        {
+            AssertMatches("as", "abc");
+        }
+
+        private void AssertNoMatches(string searchString, string longString)
+        {
+            var matcher = new FuzzyMatcher();
+
+            var results = matcher.Find(searchString, new[] { longString });
+
+            results.ShouldNotBeNull();
+            results.Count().ShouldEqual(0);
+        }
+
+        private void AssertMatches(string searchString, string longString)
+        {
+            var matcher = new FuzzyMatcher();
+
+            var results = matcher.Find(searchString, new[] { longString });
+
+            results.ShouldNotBeNull();
+            results.Count().ShouldBeGreaterThan(0);
+            results.First().ShouldEqual(longString);
+        }
+    }
+
+    public class FuzzyMatcher
+    {
+        public string[] Find(string searchString, string[] strings)
+        {
+            return strings
+                .Where(x => searchString.All(x.Contains))
+                .ToArray();
+        }
+    }
+}
---
+++ Source/AlphaLaunch.Experiments/TestExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AlphaLaunch.Experiments
+{
+    public static class TestExtensions
+    {
+    }
+}
