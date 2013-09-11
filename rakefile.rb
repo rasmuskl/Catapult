84334c798452cd493d5b22d4625b867e7cb4c184
Rasmus Kromann-Larsen <rasmus@kromann-larsen.dk>
2013-09-11 22:03:09 +0200

    Added package and curl upload.

--- rakefile.rb
+++ rakefile.rb
@@ -4,19 +4,20 @@ def get_build_number
 	yearversion = Time.now.strftime("%Y")
 	dateversion = Time.now.strftime("%m%d")
 	buildnumber = Time.now.strftime("%H%M")
-	#buildnumber = ENV["BUILD_NUMBER"].nil? ? '0' : ENV["BUILD_NUMBER"]
 	"#{$major_version}.#{yearversion}.#{dateversion}.#{buildnumber}"
 end
 
 $msbuildpath = 'C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe'
-$aspnet_compiler_path = 'C:\Windows\Microsoft.NET\Framework\v4.0.30319\aspnet_compiler.exe'
-$publishdir = 'Publish'
 $major_version = '1'
 $build_number = get_build_number
 
+$publishdir = 'Publish'
+$builddir = 'Source/AlphaLaunch.App/bin/Release/app.publish'
+$includePattern = '/**/*'
+
 task :default => [:full]
 
-task :full => [:clean, :compile, :publish] do
+task :full => [:clean, :compile, :publish, :copyfiles] do
 	puts $build_number
 end
 
@@ -50,3 +51,21 @@ msbuild :publish => [:compile] do |msb|
 	msb.solution = "Source/AlphaLaunch.App.sln"	
 end
 
+task :copyfiles => [:publish] do
+	rm_rf 'Publish'
+	mkdir 'Publish'
+	FileList[$builddir + $includePattern].each do |f|
+		targetLocation = f.sub($builddir, $publishdir)
+		mkdir_p File.dirname(targetLocation)
+		if not File.directory?(f)
+			cp f, targetLocation
+		end
+	end
+end
+
+task :uploadfiles do
+	FileList[$publishdir + $includePattern].each do |f|
+		targetLocation = f.sub($publishdir, 'setup')
+		system 'tools/curl/curl.exe "ftp://alphalaunch.rasmuskl.dk/Alphalaunch/'+targetLocation+'" --ssl --insecure --ftp-create-dirs --user ' + ENV['ALPHALAUNCH_FTP'] + ' -T "'+f+'"'
+	end
+end
