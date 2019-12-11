f479b1b46d91fbc78519f9c1003ced1cff494a1a
Rasmus Kromann-Larsen <rasmus@kromann-larsen.dk>
2019-12-11 22:02:44 +0100

    Make triple backtip clipboard string remove shared whitespace across all lines

--- Source/Catapult.Core/Actions/TripleBacktickClipboardString.cs
+++ Source/Catapult.Core/Actions/TripleBacktickClipboardString.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Catapult.Core.Indexes;
 
 namespace Catapult.Core.Actions
@@ -14,10 +16,23 @@ namespace Catapult.Core.Actions
                 return;
             }
 
-            var plingifiedText = $"```{Environment.NewLine}{text}{Environment.NewLine}```";
+            var plingifiedText = $"```{Environment.NewLine}{TrimSharedWhitespacePrefix(text)}{Environment.NewLine}```";
             TextCopy.Clipboard.SetText(plingifiedText);
         }
 
         public override string Name => "Triple backtick clipboard string";
+
+        public static string TrimSharedWhitespacePrefix(string str)
+        {
+            if (str.IsNullOrWhiteSpace())
+            {
+                return str;
+            }
+
+            IEnumerable<string> lines = str.Split(new [] { Environment.NewLine }, StringSplitOptions.None);
+            var min = lines.Where(x => !string.IsNullOrWhiteSpace(x)).Min(x => x.TakeWhile(char.IsWhiteSpace).Count());
+            lines = lines.Select(x => !string.IsNullOrWhiteSpace(x) ? x.Substring(min) : x);
+            return string.Join(Environment.NewLine, lines);
+        }
     }
 }
---
+++ Source/Catapult.Tests/TripleBacktickClipboardStringTests.cs
@@ -0,0 +1,87 @@
+﻿using Catapult.Core.Actions;
+using NUnit.Framework;
+using Should;
+
+namespace Catapult.Tests
+{
+    [TestFixture]
+    public class TripleBacktickClipboardStringTests
+    {
+        [Test]
+        public void TrimSharedWhitespacePrefix_NoWhitespaceNoChange()
+        {
+            var test = TripleBacktickClipboardString.TrimSharedWhitespacePrefix("test");
+            test.ShouldEqual("test");
+        }
+
+        [Test]
+        public void TrimSharedWhitespacePrefix_SimpleWhitespaceRemoved()
+        {
+            var test = TripleBacktickClipboardString.TrimSharedWhitespacePrefix("   test");
+            test.ShouldEqual("test");
+        }
+
+        [Test]
+        public void TrimSharedWhitespacePrefix_TabWhitespaceRemoved()
+        {
+            var test = TripleBacktickClipboardString.TrimSharedWhitespacePrefix("   test");
+            test.ShouldEqual("test");
+        }
+
+        [Test]
+        public void TrimSharedWhitespacePrefix_MultilineWhitespaceRemoved()
+        {
+            var test = TripleBacktickClipboardString.TrimSharedWhitespacePrefix(@"   test
+   test");
+            test.ShouldEqual(@"test
+test");
+        }
+
+        [Test]
+        public void TrimSharedWhitespacePrefix_MultilineWhitespaceRemovesOnlyShared()
+        {
+            var test = TripleBacktickClipboardString.TrimSharedWhitespacePrefix(@" test
+  test");
+            test.ShouldEqual(@"test
+ test");
+        }
+
+        [Test]
+        public void TrimSharedWhitespacePrefix_MultilineWhitespaceIgnoresEmptyLines()
+        {
+            var test = TripleBacktickClipboardString.TrimSharedWhitespacePrefix(@"  test
+
+  test");
+            test.ShouldEqual(@"test
+
+test");
+        }
+
+        [Test]
+        public void TrimSharedWhitespacePrefix_EmptyStringWorks()
+        {
+            var test = TripleBacktickClipboardString.TrimSharedWhitespacePrefix(@"");
+            test.ShouldEqual(@"");
+        }
+
+        [Test]
+        public void TrimSharedWhitespacePrefix_MultilineWhitespaceStringWorks()
+        {
+            var test = TripleBacktickClipboardString.TrimSharedWhitespacePrefix(@"
+
+
+");
+            test.ShouldEqual(@"
+
+
+");
+        }
+
+        [Test]
+        public void TrimSharedWhitespacePrefix_NullWorks()
+        {
+            var test = TripleBacktickClipboardString.TrimSharedWhitespacePrefix(null);
+            test.ShouldEqual(null);
+        }
+    }
+}
