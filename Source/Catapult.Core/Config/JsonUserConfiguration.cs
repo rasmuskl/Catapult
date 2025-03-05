90a3d910e2b7417dac7eeee9e2177052221bcc2c
Rasmus Kromann-Larsen <rasmus@kromann-larsen.dk>
2025-03-05 20:52:48 +0100

    Add .code-workspace

--- Source/Catapult.Core/Config/JsonUserConfiguration.cs
+++ Source/Catapult.Core/Config/JsonUserConfiguration.cs
@@ -2,33 +2,26 @@ namespace Catapult.Core.Config;
 
 public class JsonUserConfiguration
 {
-    public JsonUserConfiguration()
-    {
-        Paths = new string[0];
-        IgnoredDirectories = new string[0];
-        Extensions = new string[0];
-    }
-
-    public string[] Paths { get; set; }
-    public string[] IgnoredDirectories { get; set; }
-    public string[] Extensions { get; set; }
+    public string[] Paths { get; set; } = [];
+    public string[] IgnoredDirectories { get; set; } = [];
+    public string[] Extensions { get; set; } = [];
     public bool UseControlKey { get; set; }
 
     public static JsonUserConfiguration BuildDefaultSettings()
     {
         return new JsonUserConfiguration
         {
-            Paths = new[]
-            {
+            Paths =
+            [
                 Environment.GetFolderPath(Environment.SpecialFolder.StartMenu),
                 Environment.GetFolderPath(Environment.SpecialFolder.CommonStartMenu),
                 Environment.GetFolderPath(Environment.SpecialFolder.Favorites),
                 Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
                 Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
-                @"c:\dev",
-            },
-            IgnoredDirectories = new[] { "node_modules", ".git", "scratch" },
-            Extensions = new[] { ".lnk", ".exe", ".sln", "*.slnx", "*.slnf", ".url", ".docx", ".xlsx", ".pptx", ".pdf" },
+                @"c:\dev"
+            ],
+            IgnoredDirectories = ["node_modules", ".git", "scratch"],
+            Extensions = [".lnk", ".exe", ".sln", ".slnx", ".slnf", ".code-workspace", ".url", ".docx", ".xlsx", ".pptx", ".pdf"],
             UseControlKey = false
         };
     }
